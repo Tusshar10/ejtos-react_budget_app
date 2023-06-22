8ef07f6e834a5cbb9fe8247ea6101f37c72c3389
Tusshar10 <131467401+Tusshar10@users.noreply.github.com>
2023-06-22 17:43:17 +0530

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,32 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    const changebudget=(q)=>
+    {
+        if(q>20000)
+        {
+            alert('Budget should not be more than 20000');
+            return;
+        }
+        else if(q<totalExpenses)
+        {
+            alert(`Budget can't be less than spending.`);
+            return;
+        }
+        dispatch({
+            type: 'SET_BUDGET',
+            payload:q
+        });
+    }
+    const { budget,dispatch,currency } = useContext(AppContext);
+    const { expenses } = useContext(AppContext);
+    const totalExpenses = expenses.reduce((total, item) => {
+        return (total += item.cost);
+    }, 0);
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget: {currency}
+                <input type="number" step="10" value={budget} onChange={(e)=>changebudget(e.target.value)}/></span>
         </div>
     );
 };
